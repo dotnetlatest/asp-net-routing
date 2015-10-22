9a7c3da64ecda421915dba3dad1165e8dc9f6769
Samuel Mensah <samuel.nimako-mensah@popagency.com>
2015-10-21 21:36:12 -0700

    added controller routes and contraints

--- src/RoutingDemo.Site/App_Start/RouteConfig.cs
+++ src/RoutingDemo.Site/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace RoutingDemo.Site
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ src/RoutingDemo.Site/Controllers/BlogController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoutingDemo.Site.Controllers
+{
+    //Controller
+    [RoutePrefix("blog")]
+    public class BlogController : Controller
+    {
+        [Route("~/")]
+        [Route("blog")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("{blogtitle}")]
+        public ActionResult Post(string blogtitle)
+        {
+            ViewBag.Title = blogtitle;
+            return View();
+        }
+
+        [Route("{id:int}")]
+        public ActionResult Post(int id)
+        {
+            ViewBag.Title = id;
+            return View();
+        }
+        
+    }
+}
